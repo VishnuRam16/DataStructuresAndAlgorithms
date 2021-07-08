3ff492dea75309ca2a17200f41a2b43587c70d9e
VishnuRam16 <vrm6007@gmail.com>
2021-07-08 18:59:38 +0530

    Coded for avoiding collisions

--- HashTables/Hashtable.java
+++ HashTables/Hashtable.java
@@ -2,11 +2,11 @@ package HashTables;
 
 public class Hashtable 
 {
-  private StudentDetails[] hashtable;
+  private KeyValuePairing[] hashtable;
 
   public Hashtable()
   {
-    hashtable = new StudentDetails[8];
+    hashtable = new KeyValuePairing[10];
   }
 
   private int HashKey(String key)
@@ -17,28 +17,94 @@ public class Hashtable
   public void Put(String key, StudentDetails studentDetails)
   {
     int hashedKey = HashKey(key); 
-    if(hashtable[hashedKey] != null)
+    if(Occupied(hashedKey))
+    {
+      int stopIndex = hashedKey;
+      if(hashedKey == hashtable.length - 1)
+      {
+        hashedKey = 0;
+      }
+      else
+      {
+        hashedKey++;
+      }
+      while(Occupied(hashedKey) && hashedKey != stopIndex)
+      {
+        hashedKey = (hashedKey + 1) % hashtable.length;
+      }
+
+    }
+
+    if(Occupied(hashedKey))
     {
       System.out.println("Position filled at: " +hashedKey);
     }
     else
     {
-      hashtable[hashedKey] = studentDetails;
+      hashtable[hashedKey] = new KeyValuePairing(key, studentDetails);
     }
+
   }
 
   public StudentDetails Retrieve(String key)
   {
-    int hashedKey = HashKey(key); 
-    return hashtable[hashedKey];
+    int hashedKey = FindKey(key); 
+    if(hashedKey == -1)
+    {
+      return null;
+    }
+    return hashtable[hashedKey].studentDetails;
   }
 
   public void DisplayHashTable()
   {
     for(int i =0; i < hashtable.length; i++)
+    if(hashtable[i] == null)
     {
-      System.out.println(hashtable[i]);
+      System.out.println("Empty");
     }
+    else
+    {
+      System.out.println(hashtable[i].studentDetails);
+    }
+  }
+
+  public boolean Occupied(int index)
+  {
+    return hashtable[index] != null;
+  }
+
+  private int FindKey(String key)
+  {
+    int hashedKey = HashKey(key);
+    if(hashtable[hashedKey] != null && hashtable[hashedKey].key.equals(key))
+    {
+      return hashedKey;
+    }
+  
+    int stopIndex = hashedKey;
+    if(hashedKey == hashtable.length - 1)
+    {
+      hashedKey = 0;
+    }
+    else
+    {
+      hashedKey++;
+    }
+    while(hashedKey != stopIndex && hashtable[hashedKey] != null && !hashtable[hashedKey].key.equals(key))
+    {
+      hashedKey = (hashedKey + 1) % hashtable.length;
+    }
+
+    if(stopIndex == hashedKey)
+    {
+      return -1;
+    }
+    else
+    {
+      return hashedKey;
+    }
+    
   }
 
   
---
+++ HashTables/KeyValuePairing.java
@@ -0,0 +1,14 @@
+package HashTables;
+
+public class KeyValuePairing 
+{
+  public String key;
+  public StudentDetails studentDetails;
+  
+  public KeyValuePairing(String key, StudentDetails studentDetails)
+  {
+    this.key = key;
+    this.studentDetails = studentDetails;
+  }
+  
+}
