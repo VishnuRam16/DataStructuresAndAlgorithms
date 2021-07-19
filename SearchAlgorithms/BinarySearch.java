2a6f326e8a2e5822f59eb7bbcc1d629ea5c558c0
VishnuRam16 <vrm6007@gmail.com>
2021-07-19 17:30:05 +0530

    Coded binary search with recursion

--- SearchAlgorithms/BinarySearch.java
+++ SearchAlgorithms/BinarySearch.java
@@ -3,31 +3,34 @@ package SearchAlgorithms;
 public class BinarySearch 
 {
 
-  public static int binarySearch(int[] arr, int value)
+  public static int recursiveBinarySearch(int[] arr, int value)
   {
-    int start = 0;
-    int end = arr.length;
+    return recursiveBinarySearch(arr, 0, arr.length, value);
+  }
 
-    while(start < end)
+  public static int recursiveBinarySearch(int[] arr, int start, int end, int value)
+  {
+    if(start >= end)
     {
-      int midPoint = (start + end) / 2;
-      System.out.println("MidPoint is = "+midPoint);
-
-      if(arr[midPoint] == value)
-      {
-        return midPoint;
-      }
-      else if(arr[midPoint] < value)
-      {
-          start = midPoint + 1;
-      }
-      else
-      {
-          end = midPoint;
-      }
+      return -1;
     }
 
-    return -1;
+    int midPoint = (start + end) / 2;
+    System.out.println("MidPoint is = "+midPoint);
+
+    if(arr[midPoint] == value)
+    {
+      return midPoint;
+    }
+    else if(arr[midPoint] < value)
+    {
+      return recursiveBinarySearch(arr, midPoint + 1, end, value);
+    }
+    else
+    {
+      return recursiveBinarySearch(arr, start, midPoint, value);
+    }
+    
 
   }
 
@@ -35,6 +38,6 @@ public class BinarySearch
   {
     int[] array = {1,4,5,8,9,10,12};
 
-    System.out.println("Required element is at position : " +binarySearch(array, 9));
+    System.out.println("Required element is at position : " +recursiveBinarySearch(array, 10));
   }
 }
