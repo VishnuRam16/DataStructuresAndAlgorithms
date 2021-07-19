e4b6033198b1e5f008167806e6d20206fd1227f5
VishnuRam16 <vrm6007@gmail.com>
2021-07-19 16:30:53 +0530

    Coded a simple linear search algorithm

---
+++ SearchAlgorithms/LinearSearch.java
@@ -0,0 +1,25 @@
+package SearchAlgorithms;
+
+public class LinearSearch 
+{
+
+  public static int linearSearch(int[] arr, int value)
+  {
+    for(int i = 0; i < arr.length; i++)
+    {
+      if(arr[i] == value)
+      {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+  public static void main(String args[])
+  {
+    int[] array = {10, 20, 5, -10, 12, 4, 0, 24};
+
+    System.out.println("Required element is at index: ");
+    System.out.println(linearSearch(array, 12));
+  }
+}
